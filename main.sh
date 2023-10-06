ea5cd2200b39c3577755000fbdcea9c590147a25
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-06 22:27:30 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # add debs to repo
-ssh ferreo@direct.pika-os.com 'aptly repo remove pikauwu-main 'Name (% libwebrtc-audio-processing-dev)' '
-ssh ferreo@direct.pika-os.com 'aptly repo remove pikauwu-main 'Name (% libwebrtc-audio-processing1)' '
+ssh ferreo@direct.pika-os.com 'aptly repo remove pikauwu-main '"'Name (% libwebrtc-audio-processing-dev)'"' '
+ssh ferreo@direct.pika-os.com 'aptly repo remove pikauwu-main '"'Name (% libwebrtc-audio-processing1)'"' '
 # publish the repo
 ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite pikauwu filesystem:pikarepo:'
